a1d50d19de9fd9ba200579c63e5fa428854b437c
Deeprodigy-hub <divineyabine2019@gmail.com>
2024-07-18 21:10:14 +0100

    upload

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: "Which club has won the most UEFA Champions League titles?",
+      option1: "Barcelona",
+      option2: "Bayern Munich",
+      option3: "Liverpool",
+      option4: "Real Madrid",
+      ans: 4,
+    },
+    {
+      question: "Who is the all-time top scorer in the UEFA Champions League?",
+      option1: "Lionel Messi",
+      option2: "Cristiano Ronaldo",
+      option3: "Robert Lewandowski",
+      option4: "Karim Benzema",
+      ans: 2,
+    },
+    {
+      question: "Which team won the UEFA Champions League in 2021?",
+      option1: "Manchester City",
+      option2: "Paris Saint-Germain",
+      option3: "Chelsea",
+      option4: "Bayern Munich",
+      ans: 3,
+    },
+    {
+      question: "Who scored the winning goal in the 2014 UEFA Champions League final?",
+      option1: "Cristiano Ronaldo",
+      option2: "Gareth Bale",
+      option3: "Sergio Ramos",
+      option4: "Ángel Di María",
+      ans: 2,
+    },
+    {
+      question: "Which manager has won the most UEFA Champions League titles?",
+      option1: "Pep Guardiola",
+      option2: "Jose Mourinho",
+      option3: "Carlo Ancelotti",
+      option4: "Zinedine Zidane",
+      ans: 3,
+    }
+  ];
+  
